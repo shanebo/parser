09a28c11036d4bf06425849c7519234053c68f32
Shane Thacker <shane@steadymade.com>
2019-09-29 01:32:36 -0500

    Add robust coercion

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 const qs = require('qs');
+const { coerce } = require('./coerce');
 
-module.exports = () => {
+
+module.exports = (options) => {
+  const defaults = {
+    coerce: true,
+    allowDots: true,
+    arrayFormat: 'repeat'
+  };
+
+  const opts = { ...defaults, ...options };
+
+  // Object.defineProperty(httprequest, 'query', {
+  //   get() {
+  //     super();
+  //     this._query = coerceValues(this._query);
+  //     coerceParams(this.search, this._query);
+  //     return this._query;
+  //   }
+  //   set(val) {
+  //     super(val);
+  //     return this._query;
+  //   }
+  // })
+
+
   return (req, res, next) => {
+    // implement object define instead so it only runs when app calls it
+    if (req.querystring) {
+      req.query = coerce(req.query, opts, req.querystring);
+    }
+
     if (/^(POST|PUT|PATCH|DELETE)$/i.test(req.method)) {
       let body = '';
+
       req.on('data', (data) => {
         body += data;
       });
 
       req.on('end', () => {
-        req.body = req.is('json')
-          ? JSON.parse(body)
-          : qs.parse(body);
+        if (body) {
+          req.body = req.is('json')
+            ? JSON.parse(body)
+            : qs.parse(body, opts)
+
+          req.body = coerce(req.body, opts, decodeURIComponent(body));
+        }
+
         next();
       });
     } else {
